baa5cce846d21fd505fec5987abcd43635effedc
zhy <zhy@123.com>
2025-08-11 00:06:27 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,18 +3,18 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -80,6 +80,17 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
         System.out.println("当前线程id:"+Thread.currentThread().getId());
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("员工分页查询")
+    /**
+     * 分页查询
+     */
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+    log.info("员工分页查询,参数为：{}",employeePageQueryDTO);
+    PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+    return Result.success(pageResult);
     }
 
 }
